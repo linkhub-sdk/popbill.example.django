4c15afa28cbe0e9114e3bf6e7d910a9a4d70c1a2
kimhyunjin <khjzzm@gmail.com>
2019-01-16 11:26:20 +0900

    HTTaxinvoce code clean

--- HTTaxinvoiceExample/views.py
+++ HTTaxinvoiceExample/views.py
@@ -11,6 +11,10 @@ htTaxinvoiceService = HTTaxinvoiceService(settings.LinkID, settings.SecretKey)
 htTaxinvoiceService.IsTest = settings.IsTest
 
 
+# 홈택스 연동서비스를 이용하기 위해 인증 처리를 합니다. (인증방법은 사이트/API 두가지 방식이 있습니다.)
+# 1. 팝빌로그인 > [홈택스연동] > [환경설정] > [인증 관리] 메뉴에서 홈택스 인증 처리를 합니다.
+# 2. 홈택스연동 인증 관리 팝업 URL(GetCertificatePopUpURL API) 반환된 URL을 이용하여 홈택스 인증 처리를 합니다.
+
 def index(request):
     return render(request, 'HTTaxinvoice/Index.html', {})
 
@@ -18,8 +22,8 @@ def index(request):
 def requestJob(request):
     """
     전자(세금)계산서 매출/매입 내역 수집을 요청합니다
-    - 매출/매입 연계 프로세스는 "[홈택스 전자(세금)계산서 연계 API 연동매뉴얼]
-      > 1.API소개 (프로세스 흐름도)" 를 참고하시기 바랍니다.
+    - 홈택스연동 프로세스는 "[홈택스연동(전자세금계산서계산서) API 연동매뉴얼] >
+      1.1. 홈택스연동(전자세금계산서) API 구성" 을 참고하시기 바랍니다.
     - 수집 요청후 반환받은 작업아이디(JobID)의 유효시간은 1시간 입니다.
     """
     try:
@@ -29,17 +33,17 @@ def requestJob(request):
         # 팝빌회원 아이디
         UserID = settings.testUserID
 
-        # 전자세금계산서  발행유형, [SELL-매출 / BUY-매입 / TRUSTEE-위수탁]
+        # 전자세금계산서  발행유형 [SELL-매출 / BUY-매입 / TRUSTEE-위수탁]
         Type = "SELL"
 
         # 일자유형, [W-작성일자 / I-발행일자 / S-전송일자]
         DType = "W"
 
         # 시작일자, 날짜형식(yyyyMMdd)
-        SDate = "20171201"
+        SDate = "20190101"
 
         # 종료일자, 날짜형식(yyyyMMdd)
-        EDate = "20180131"
+        EDate = "20190116"
 
         result = htTaxinvoiceService.requestJob(CorpNum, Type, DType, SDate, EDate, UserID)
 
@@ -51,8 +55,8 @@ def requestJob(request):
 def getJobState(request):
     """
     수집 요청 상태를 확인합니다.
-    - 응답항목 관한 정보는 "[홈택스 전자(세금)계산서 연계 API 연동매뉴얼
-      > 3.1.2. GetJobState(수집 상태 확인)" 을 참고하시기 바랍니다 .
+    - 응답항목 관한 정보는 "[홈택스연동 (전자세금계산서계산서) API 연동매뉴얼] >
+      3.1.2. GetJobState(수집 상태 확인)" 을 참고하시기 바랍니다.
     """
     try:
         # 팝빌회원 사업자번호
@@ -75,8 +79,8 @@ def listActiveJob(request):
     """
     수집 요청건들에 대한 상태 목록을 확인합니다.
     - 수집 요청 작업아이디(JobID)의 유효시간은 1시간 입니다.
-    - 응답항목에 관한 정보는 "[홈택스 전자(세금)계산서 연계 API 연동매뉴얼]
-      > 3.1.3. ListActiveJob (수집 상태 목록 확인)" 을 참고하시기 바랍니다.
+    - 응답항목에 관한 정보는 "[홈택스연동 (전자세금계산서계산서) API 연동매뉴얼] >
+      3.1.3. ListActiveJob(수집 상태 목록 확인)" 을 참고하시기 바랍니다.
     """
     try:
         # 팝빌회원 사업자번호
@@ -94,9 +98,9 @@ def listActiveJob(request):
 
 def search(request):
     """
-    검색조건을 사용하여 수집결과를 조회합니다.
-    - 응답항목에 관한 정보는 "[홈택스 전자(세금)계산서 연계 API 연동매뉴얼]
-      > 3.2.1. Search (수집 결과 조회)" 을 참고하시기 바랍니다.
+    전자세금계산서 매입/매출 내역의 수집 결과를 조회합니다.
+    - 응답항목에 관한 정보는 "[홈택스연동 (전자세금계산서계산서) API 연동매뉴얼] >
+      3.2.1. Search(수집 결과 조회)" 을 참고하시기 바랍니다.
     """
     try:
         # 팝빌회원 사업자번호
@@ -146,8 +150,8 @@ def search(request):
 def summary(request):
     """
     검색조건을 사용하여 수집 결과 요약정보를 조회합니다.
-    - 응답항목에 관한 정보는 "[홈택스 전자(세금)계산서 연계 API 연동매뉴얼]
-      > 3.2.2. Summary (수집 결과 요약정보 조회)" 을 참고하시기 바랍니다.
+    - 응답항목에 관한 정보는 "[홈택스연동 (전자세금계산서계산서) API 연동매뉴얼] >
+      3.2.2. Summary(수집 결과 요약정보 조회)" 을 참고하시기 바랍니다.
     """
     try:
         # 팝빌회원 사업자번호
@@ -187,9 +191,9 @@ def summary(request):
 
 def getTaxinvoice(request):
     """
-    수집된 전자(세금)계산서 1건의 상세정보를 확인합니다.
-    - 응답항목에 관한 정보는 "[홈택스 전자(세금)계산서 연계 API 연동매뉴얼]
-      > 4.2.2. GetTaxinvoice 응답전문 구성" 을 참고하시기 바랍니다.
+    전자세금계산서 1건의 상세정보를 확인합니다.
+    - 응답항목에 관한 정보는 "[홈택스연동 (전자세금계산서계산서) API 연동매뉴얼] >
+      4.1.2. GetTaxinvoice 응답전문 구성" 을 참고하시기 바랍니다.
     """
     try:
         # 팝빌회원 사업자번호
@@ -210,9 +214,9 @@ def getTaxinvoice(request):
 
 def getXML(request):
     """
-    XML형식의 전자(세금)계산서 상세정보를 1건을 확인합니다.
-    - 응답항목에 관한 정보는 "[홈택스 전자(세금)계산서 연계 API 연동매뉴얼]
-      > 3.2.4. GetXML (상세정보 확인 - XML)" 을 참고하시기 바랍니다.
+    XML 형식의 전자세금계산서 상세정보를 확인합니다.
+    - 응답항목에 관한 정보는 "[홈택스연동 (전자세금계산서계산서) API 연동매뉴얼] >
+      3.2.4. GetXML(상세정보 확인 - XML)" 을 참고하시기 바랍니다.
     """
     try:
         # 팝빌회원 사업자번호
@@ -231,9 +235,26 @@ def getXML(request):
         return render(request, 'exception.html', {'code': PE.code, 'message': PE.message})
 
 
+def getPopUpURL(request):
+    """
+    홈택스 전자세금계산서 보기 팝업 URL을 반환 합니다.
+    - 보안정책에 의해 응답된 URL은 30초의 만료시간을 갖습니다.
+    """
+    try:
+        # 팝빌회원 사업자번호
+        CorpNum = settings.testCorpNum
+
+        url = htTaxinvoiceService.getPopUpURL(CorpNum)
+
+        return render(request, 'url.html', {'url': url})
+    except PopbillException as PE:
+        return render(request, 'exception.html', {'code': PE.code, 'message': PE.message})
+
+
 def getCertificatePopUpURL(request):
     """
-    팝빌에 로그인 하지 않고 홈택스 공인인증서를 등록할 수 있는 팝업 URL을 반환합니다.
+    홈택스연동 인증관리를 위한 URL을 반환합니다.
+    - 인증방식에는 부서사용자/공인인증서 인증 방식이 있습니다.
     - 보안정책에 의해 응답된 URL은 30초의 만료시간을 갖습니다.
     """
     try:
@@ -262,6 +283,87 @@ def getCertificateExpireDate(request):
         expiredate = htTaxinvoiceService.getCertificateExpireDate(CorpNum, UserID)
 
         return render(request, 'HTTaxinvoice/GetCertificateExpireDate.html', {'expiredate': expiredate})
+    except PopbillException as PE:
+        return render(request, 'exception.html', {'code': PE.code, 'message': PE.message})
+
+
+def checkCertValidation(request):
+    """
+    팝빌에 등록된 공인인증서의 홈택스 로그인을 테스트합니다.
+    """
+    try:
+        # 팝빌회원 사업자번호
+        CorpNum = settings.testCorpNum
+
+        response = htTaxinvoiceService.checkCertValidation(CorpNum)
+
+        return render(request, 'response.html', {'code': response.code, 'message': response.message})
+    except PopbillException as PE:
+        return render(request, 'exception.html', {'code': PE.code, 'message': PE.message})
+
+
+def registDeptUser(request):
+    """
+    홈택스 전자세금계산서 부서사용자 계정을 등록합니다.
+    """
+    try:
+        # 팝빌회원 사업자번호
+        CorpNum = settings.testCorpNum
+
+        # 홈택스 부서사용자 계정아이디
+        DeptUserID = "deptuserid"
+
+        # 홈택스 부서사용자 계정비밀번호
+        DeptUserPWD = "deptuserpwd"
+
+        response = htTaxinvoiceService.registDeptUser(CorpNum, DeptUserID, DeptUserPWD)
+
+        return render(request, 'response.html', {'code': response.code, 'message': response.message})
+    except PopbillException as PE:
+        return render(request, 'exception.html', {'code': PE.code, 'message': PE.message})
+
+
+def checkDeptUser(request):
+    """
+    팝빌에 등록된 전자세금계산서 부서사용자 아이디를 확인합니다.
+    """
+    try:
+        # 팝빌회원 사업자번호
+        CorpNum = settings.testCorpNum
+
+        response = htTaxinvoiceService.checkDeptUser(CorpNum)
+
+        return render(request, 'response.html', {'code': response.code, 'message': response.message})
+    except PopbillException as PE:
+        return render(request, 'exception.html', {'code': PE.code, 'message': PE.message})
+
+
+def checkLoginDeptUser(request):
+    """
+    팝빌에 등록된 전자세금계산서 부서사용자 계정정보를 이용하여 홈택스 로그인을 테스트합니다.
+    """
+    try:
+        # 팝빌회원 사업자번호
+        CorpNum = settings.testCorpNum
+
+        response = htTaxinvoiceService.checkLoginDeptUser(CorpNum)
+
+        return render(request, 'response.html', {'code': response.code, 'message': response.message})
+    except PopbillException as PE:
+        return render(request, 'exception.html', {'code': PE.code, 'message': PE.message})
+
+
+def deleteDeptUser(request):
+    """
+    팝빌에 등록된 전자세금계산서 부서사용자 계정정보를 삭제합니다.
+    """
+    try:
+        # 팝빌회원 사업자번호
+        CorpNum = settings.testCorpNum
+
+        response = htTaxinvoiceService.deleteDeptUser(CorpNum)
+
+        return render(request, 'response.html', {'code': response.code, 'message': response.message})
     except PopbillException as PE:
         return render(request, 'exception.html', {'code': PE.code, 'message': PE.message})
 
@@ -338,7 +440,7 @@ def getPartnerURL(request):
 
 def getChargeInfo(request):
     """
-    연동회원의 현금영수증 API 서비스 과금정보를 확인합니다.
+    연동회원의 홈택스연동 API 서비스 과금정보를 확인합니다.
     """
     try:
         # 팝빌회원 사업자번호
@@ -429,44 +531,44 @@ def joinMember(request):
         # 회원정보
         newMember = JoinForm(
 
-            # 회원아이디, 최대 20자
-            ID="testkorea",
+            # 아이디 (6자 이상 50자 미만)
+            ID="join_id_test",
 
-            # 비밀번호, 최대 20자
+            # 비밀번호 (6자 이상 20자 미만)
             PWD="this_is_password",
 
-            # 사업자번호
-            CorpNum="1234567890",
+            # 사업자번호 "-" 제외
+            CorpNum="0000000000",
 
-            # 상호
-            CorpName="테스트가입상호",
-
-            # 대표자성명
+            # 대표자성명 (최대 100자)
             CEOName="테스트대표자성명",
 
-            # 주소
-            Addr="테스트 회사 주소",
+            # 상호 (최대 200자)
+            CorpName="테스트가입상호",
 
-            # 업태
+            # 주소 (최대 300자)
+            Addr="테스트회사주소",
+
+            # 업태 (최대 100자)
             BizType="테스트업태",
 
-            # 종목
+            # 종목 (최대 100자)
             BizClass="테스트업종",
 
-            # 담당자 성명
+            # 담당자 성명 (최대 100자)
             ContactName="담당자성명",
 
-            # 담당자 연락처
-            ContactTEL="070-4304-2991",
+            # 담당자 이메일주소 (최대 100자)
+            ContactEmail="test@test.com",
 
-            # 담당자 휴대폰번호
-            ContactHP="010-2222-3333",
+            # 담당자 연락처 (최대 20자)
+            ContactTEL="070-111-222",
 
-            # 담당자 팩스번호
-            ContactFAX="070-4304-2991",
+            # 담당자 휴대폰번호 (최대 20자)
+            ContactHP="010-111-222",
 
-            # 담당자 메일주소
-            ContactEmail="test@test.com"
+            # 담당자 팩스번호 (최대 20자)
+            ContactFAX="070-111-222"
         )
 
         response = htTaxinvoiceService.joinMember(newMember)
@@ -509,29 +611,32 @@ def registContact(request):
         # 담당자 정보
         newContact = ContactInfo(
 
-            # 아이디
-            id="testkorea_1117",
+            # 아이디 (6자 이상 50자 미만)
+            id="popbill_test_id",
 
-            # 비밀번호
-            pwd="this_is_password",
+            # 비밀번호 (6자 이상 20자 미만)
+            pwd="popbill_test_pwd",
 
-            # 담당자명
-            personName="정대리",
+            # 담당자명 (최대 100자)
+            personName="담당자명",
 
-            # 연락처
-            tel="010-4304-2991",
+            # 담당자 연락처 (최대 20자)
+            tel="010-111-222",
 
-            # 휴대폰번호
-            hp="010-4304-2991",
+            # 담당자 휴대폰번호 (최대 20자)
+            hp="010-111-222",
 
-            # 팩스번호
-            fax="070-4324-2991",
+            # 담당자 팩스번호 (최대 20자)
+            fax="070-111-222",
 
-            # 메일주소
-            email="dev@linkhub.co.kr",
+            # 담당자 이메일 (최대 100자)
+            email="test@test.com",
 
             # 회사조회 권한여부, True(회사조회) False(개인조회)
-            searchAllAllowYN=True
+            searchAllAllowYN=True,
+
+            # 관리자 권한여부, True(관리자), False(사용자)
+            mgrYN=True
         )
 
         response = htTaxinvoiceService.registContact(CorpNum, newContact, UserID)
@@ -573,19 +678,19 @@ def updateCorpInfo(request):
         # 회사정보
         corpInfo = CorpInfo(
 
-            # 대표자성명
-            ceoname="대표자성명",
+            # 대표자 성명 (최대 100자)
+            ceoname="대표자_성명",
 
-            # 상호
+            # 상호 (최대 200자)
             corpName="상호",
 
-            # 주소
+            # 주소 (최대 300자)
             addr="주소",
 
-            # 업태
+            # 업태 (최대 100자)
             bizType="업태",
 
-            # 종목
+            # 종목 (최대 100자)
             bizClass="종목"
         )
 
@@ -632,125 +737,31 @@ def updateContact(request):
         updateInfo = ContactInfo(
 
             # 담당자 아이디
-            id=UserID,
+            id="UserID",
 
-            # 담당자 성명
-            personName="담당자 성명",
+            # 담당자 성명 (최대 100자)
+            personName="담당자_성명",
 
-            # 연락처
-            tel="070-4304-2991",
+            # 담당자 연락처 (최대 20자)
+            tel="010-111-111",
 
-            # 휴대폰번호
-            hp="010-4324-4324",
+            # 담당자 휴대폰번호 (최대 20자)
+            hp="010-111-111",
 
-            # 팩스번호
+            # 담당자 팩스번호 (최대 20자)
             fax="070-111-222",
 
-            # 메일주소
-            email="dev@linkhub.co.kr",
+            # 담당자 메일주소 (최대 100자)
+            email="test@test.com",
 
-            # 회사조회 여부, True-회사조회, False-개인조회
-            searchAllAllowYN=True
+            # 회사조회 권한여부, True(회사조회) False(개인조회)
+            searchAllAllowYN=True,
+
+            # 관리자 권한여부, True(관리자), False(사용자)
+            mgrYN=True
         )
 
         response = htTaxinvoiceService.updateContact(CorpNum, updateInfo, UserID)
-
-        return render(request, 'response.html', {'code': response.code, 'message': response.message})
-    except PopbillException as PE:
-        return render(request, 'exception.html', {'code': PE.code, 'message': PE.message})
-
-
-def getPopUpURL(request):
-    """
-    홈택스 전자세금계산서 보기 팝업 URL을 반환 합니다.
-    - 보안정책에 의해 응답된 URL은 30초의 만료시간을 갖습니다.
-    """
-    try:
-        # 팝빌회원 사업자번호
-        CorpNum = settings.testCorpNum
-
-        url = htTaxinvoiceService.getPopUpURL(CorpNum)
-
-        return render(request, 'url.html', {'url': url})
-    except PopbillException as PE:
-        return render(request, 'exception.html', {'code': PE.code, 'message': PE.message})
-
-
-def checkCertValidation(request):
-    """
-    팝빌에 등록된 공인인증서의 홈택스 로그인을 테스트합니다.
-    """
-    try:
-        # 팝빌회원 사업자번호
-        CorpNum = settings.testCorpNum
-
-        response = htTaxinvoiceService.checkCertValidation(CorpNum)
-
-        return render(request, 'response.html', {'code': response.code, 'message': response.message})
-    except PopbillException as PE:
-        return render(request, 'exception.html', {'code': PE.code, 'message': PE.message})
-
-
-def registDeptUser(request):
-    """
-    홈택스 전자세금계산서 부서사용자 계정을 등록합니다.
-    """
-    try:
-        # 팝빌회원 사업자번호
-        CorpNum = settings.testCorpNum
-
-        # 홈택스 부서사용자 계정아이디
-        DeptUserID = "deptuserid"
-
-        # 홈택스 부서사용자 계정비밀번호
-        DeptUserPWD = "deptuserpwd"
-
-        response = htTaxinvoiceService.registDeptUser(CorpNum, DeptUserID, DeptUserPWD)
-
-        return render(request, 'response.html', {'code': response.code, 'message': response.message})
-    except PopbillException as PE:
-        return render(request, 'exception.html', {'code': PE.code, 'message': PE.message})
-
-
-def checkDeptUser(request):
-    """
-    팝빌에 등록된 전자세금계산서 부서사용자 아이디를 확인합니다.
-    """
-    try:
-        # 팝빌회원 사업자번호
-        CorpNum = settings.testCorpNum
-
-        response = htTaxinvoiceService.checkDeptUser(CorpNum)
-
-        return render(request, 'response.html', {'code': response.code, 'message': response.message})
-    except PopbillException as PE:
-        return render(request, 'exception.html', {'code': PE.code, 'message': PE.message})
-
-
-def checkLoginDeptUser(request):
-    """
-    팝빌에 등록된 전자세금계산서 부서사용자 계정정보를 이용하여 홈택스 로그인을 테스트합니다.
-    """
-    try:
-        # 팝빌회원 사업자번호
-        CorpNum = settings.testCorpNum
-
-        response = htTaxinvoiceService.checkLoginDeptUser(CorpNum)
-
-        return render(request, 'response.html', {'code': response.code, 'message': response.message})
-    except PopbillException as PE:
-        return render(request, 'exception.html', {'code': PE.code, 'message': PE.message})
-
-
-def deleteDeptUser(request):
-    """
-    팝빌에 등록된 전자세금계산서 부서사용자 계정정보를 삭제합니다.
-    """
-    try:
-        # 팝빌회원 사업자번호
-        CorpNum = settings.testCorpNum
-
-        response = htTaxinvoiceService.deleteDeptUser(CorpNum)
 
         return render(request, 'response.html', {'code': response.code, 'message': response.message})
     except PopbillException as PE:
