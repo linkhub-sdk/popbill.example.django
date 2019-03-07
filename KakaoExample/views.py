4ffb42e0c93dd491a2729883c94c0bc02e0bb531
JohnJeong123 <frenchofkiss@gmail.com>
2019-03-07 14:57:06 +0900

    added SendATS button list

--- KakaoExample/views.py
+++ KakaoExample/views.py
@@ -143,14 +143,19 @@ def sendATS_one(request):
 
         # 알림톡 템플릿코드
         # 승인된 알림톡 템플릿 코드는 ListATStemplate API, GetATSTemplateMgtURL API, 혹은 팝빌사이트에서 확인이 가능합니다.
-        templateCode = "018110000047"
+        templateCode = "019020000163"
 
         # 발신번호 (팝빌에 등록된 발신번호만 이용가능)
         snd = "07043042992"
 
         # 알림톡 내용 (최대 1000자)
         # 사전에 승인된 템플릿의 내용과 알림톡 전송내용(content)이 다를 경우 전송실패 처리됩니다.
-        content = "테스트 템플릿 입니다."
+        content = "[ 팝빌 ]\n"
+        content += "신청하신 #{템플릿코드}에 대한 심사가 완료되어 승인 처리되었습니다.\n"
+        content += "해당 템플릿으로 전송 가능합니다.\n\n"
+        content += "문의사항 있으시면 파트너센터로 편하게 연락주시기 바랍니다.\n\n"
+        content += "팝빌 파트너센터 : 1600-8536\n"
+        content += "support@linkhub.co.kr"
 
         # 대체문자 내용 (최대 2000byte)
         altContent = "알림톡 대체 문자"
@@ -162,7 +167,7 @@ def sendATS_one(request):
         sndDT = ""
 
         # 수신번호
-        receiver = "01012341234"
+        receiver = "01011122"
 
         # 수신자 이름
         receiverName = "partner"
@@ -172,8 +177,21 @@ def sendATS_one(request):
         # 1~36자리로 구성. 영문, 숫자, 하이픈(-), 언더바(_)를 조합하여 팝빌 회원별로 중복되지 않도록 할당.
         requestNum = ""
 
+        # 알림톡 버튼정보를 템플릿 신청시 기재한 버튼정보와 동일하게 전송하는 경우 btns를 빈 배열로 처리.
+        btns = []
+
+        # 알림톡 버튼 URL에 #{템플릿변수}를 기재한경우 템플릿변수 값을 변경하여 버튼정보 구성
+        #btns.append(
+        #     KakaoButton(
+        #         n="템플릿 안내",  # 버튼명
+        #         t="WL",  # 버튼유형 [DS-배송조회, WL-웹링크, AL-앱링크, MD-메시지전달, BK-봇키워드]
+        #         u1="https://www.popbill.com",  # [앱링크-Android, 웹링크-Mobile]
+        #         u2="http://www.popbill.com"  # [앱링크-IOS, 웹링크-PC URL]
+        #    )
+        #)
+
         receiptNum = kakaoService.sendATS(CorpNum, templateCode, snd, content, altContent,
-                                          altSendType, sndDT, receiver, receiverName, UserID, requestNum)
+                                          altSendType, sndDT, receiver, receiverName, UserID, requestNum, btns)
 
         return render(request, 'Kakao/ReceiptNum.html', {'receiptNum': receiptNum})
     except PopbillException as PE:
@@ -193,10 +211,19 @@ def sendATS_multi(request):
         UserID = settings.testUserID
 
         # 알림톡 템플릿 코드
-        templateCode = "018110000047"
+        templateCode = "019020000163"
 
         # 발신번호 (팝빌에 등록된 발신번호만 이용가능)
         snd = "07043042992"
+
+        # 알림톡 내용 (최대 1000자)
+        # 사전에 승인된 템플릿의 내용과 알림톡 전송내용(content)이 다를 경우 전송실패 처리됩니다.
+        content = "[ 팝빌 ]\n"
+        content += "신청하신 #{템플릿코드}에 대한 심사가 완료되어 승인 처리되었습니다.\n"
+        content += "해당 템플릿으로 전송 가능합니다.\n\n"
+        content += "문의사항 있으시면 파트너센터로 편하게 연락주시기 바랍니다.\n\n"
+        content += "팝빌 파트너센터 : 1600-8536\n"
+        content += "support@linkhub.co.kr"
 
         # 대체문자 유형 [공백-미전송, C-알림톡내용, A-대체문자내용]
         altSendType = "A"
@@ -211,7 +238,7 @@ def sendATS_multi(request):
                 KakaoReceiver(
                     rcv="010456456",  # 수신번호
                     rcvnm="linkhub",  # 수신자 이름
-                    msg="[테스트] 테스트 템플릿입니다. " + str(x) + "번째",  # 알림톡 내용 (최대 1000자)
+                    msg=content,  # 알림톡 내용 (최대 1000자)
                     altmsg="수신번호 010-456-456 알림톡 대체문자"  # 대체문자 내용 (최대 2000byte)
                 )
             )
@@ -221,8 +248,20 @@ def sendATS_multi(request):
         # 1~36자리로 구성. 영문, 숫자, 하이픈(-), 언더바(_)를 조합하여 팝빌 회원별로 중복되지 않도록 할당.
         requestNum = ""
 
+        # 알림톡 버튼정보를 템플릿 신청시 기재한 버튼정보와 동일하게 전송하는 경우 btns를 빈 배열로 처리.
+        btns = []
+
+        # 알림톡 버튼 URL에 #{템플릿변수}를 기재한경우 템플릿변수 값을 변경하여 버튼정보 구성
+        # btns.append(
+        #     KakaoButton(
+        #         n="템플릿 안내",  # 버튼명
+        #         t="WL",  # 버튼유형 [DS-배송조회, WL-웹링크, AL-앱링크, MD-메시지전달, BK-봇키워드]
+        #         u1="https://www.popbill.com",  # [앱링크-Android, 웹링크-Mobile]
+        #         u2="http://www.popbill.com"  # [앱링크-IOS, 웹링크-PC URL]
+        #     )
+        # )
         receiptNum = kakaoService.sendATS_multi(CorpNum, templateCode, snd, "", "",
-                                                altSendType, sndDT, KakaoMessages, UserID, requestNum)
+                                                altSendType, sndDT, KakaoMessages, UserID, requestNum, btns)
 
         return render(request, 'Kakao/ReceiptNum.html', {'receiptNum': receiptNum})
     except PopbillException as PE:
@@ -243,14 +282,19 @@ def sendATS_same(request):
 
         # 알림톡 템플릿 코드
         # 승인된 알림톡 템플릿 코드는 ListATStemplate API, GetATSTemplateMgtURL API, 혹은 팝빌사이트에서 확인이 가능합니다.
-        templateCode = "018110000047"
+        templateCode = "019020000163"
 
         # 발신번호 (팝빌에 등록된 발신번호만 이용가능)
         snd = "07043042992"
 
-        # [동보] 알림톡 내용 (최대 1000자)
+        # 알림톡 내용 (최대 1000자)
         # 사전에 승인된 템플릿의 내용과 알림톡 전송내용(content)이 다를 경우 전송실패 처리됩니다.
-        content = "[테스트] 테스트 템플릿입니다."
+        content = "[ 팝빌 ]\n"
+        content += "신청하신 #{템플릿코드}에 대한 심사가 완료되어 승인 처리되었습니다.\n"
+        content += "해당 템플릿으로 전송 가능합니다.\n\n"
+        content += "문의사항 있으시면 파트너센터로 편하게 연락주시기 바랍니다.\n\n"
+        content += "팝빌 파트너센터 : 1600-8536\n"
+        content += "support@linkhub.co.kr"
 
         # [동보] 대체문자 내용 (최대 2000byte)
         altContent = "[테스트] 알림톡 대체 문자"
@@ -276,8 +320,21 @@ def sendATS_same(request):
         # 1~36자리로 구성. 영문, 숫자, 하이픈(-), 언더바(_)를 조합하여 팝빌 회원별로 중복되지 않도록 할당.
         requestNum = ""
 
-        receiptNum = kakaoService.sendATS_multi(CorpNum, templateCode, snd, content, altContent,
-                                                altSendType, sndDT, KakaoMessages, UserID, requestNum)
+        # 알림톡 버튼정보를 템플릿 신청시 기재한 버튼정보와 동일하게 전송하는 경우 btns를 빈 배열로 처리.
+        btns = []
+
+        # 알림톡 버튼 URL에 #{템플릿변수}를 기재한경우 템플릿변수 값을 변경하여 버튼정보 구성
+        # btns.append(
+        #     KakaoButton(
+        #         n="템플릿 안내",  # 버튼명
+        #         t="WL",  # 버튼유형 [DS-배송조회, WL-웹링크, AL-앱링크, MD-메시지전달, BK-봇키워드]
+        #         u1="https://www.popbill.com",  # [앱링크-Android, 웹링크-Mobile]
+        #         u2="http://www.popbill.com"  # [앱링크-IOS, 웹링크-PC URL]
+        #     )
+        # )
+
+        receiptNum = kakaoService.sendATS_same(CorpNum, templateCode, snd, content, altContent,
+                                                altSendType, sndDT, KakaoMessages, UserID, requestNum, btns)
 
         return render(request, 'Kakao/ReceiptNum.html', {'receiptNum': receiptNum})
     except PopbillException as PE:
