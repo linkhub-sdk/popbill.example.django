ce4f4f616efbde78f1fdb464256af7b79254ba15
ProWON <pro_won45@naver.com>
2021-09-30 10:06:28 +0900

    fixed easyfinbank deleteBankAccount pram

--- EasyFinBankExample/views.py
+++ EasyFinBankExample/views.py
@@ -228,18 +228,16 @@ def deleteBankAccount(request):
         # 팝빌회원 아이디
         UserID = settings.testUserID
 
-        infoObj = BankAccountInfo(
-            # [필수] 은행코드
-            # 산업은행-0002 / 기업은행-0003 / 국민은행-0004 /수협은행-0007 / 농협은행-0011 / 우리은행-0020
-            # SC은행-0023 / 대구은행-0031 / 부산은행-0032 / 광주은행-0034 / 제주은행-0035 / 전북은행-0037
-            # 경남은행-0039 / 새마을금고-0045 / 신협은행-0048 / 우체국-0071 / KEB하나은행-0081 / 신한은행-0088 /씨티은행-0027
-            BankCode="",
+        # [필수] 은행코드
+        # 산업은행-0002 / 기업은행-0003 / 국민은행-0004 /수협은행-0007 / 농협은행-0011 / 우리은행-0020
+        # SC은행-0023 / 대구은행-0031 / 부산은행-0032 / 광주은행-0034 / 제주은행-0035 / 전북은행-0037
+        # 경남은행-0039 / 새마을금고-0045 / 신협은행-0048 / 우체국-0071 / KEB하나은행-0081 / 신한은행-0088 /씨티은행-0027
+        BankCode = ""
 
-            # [필수] 삭제할 계좌번호 하이픈('-') 제외
-            AccountNumber=""
-        )
+        # [필수] 계좌번호 하이픈('-') 제외
+        AccountNumber = ""
 
-        response = easyFinBankService.deleteBankAccount(CorpNum, infoObj, UserID)
+        response = easyFinBankService.deleteBankAccount(CorpNum, BankCode, AccountNumber, UserID)
 
         return render(request, 'response.html', {'code': response.code, 'message': response.message})
     except PopbillException as PE:
