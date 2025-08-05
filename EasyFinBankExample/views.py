2f194ee401ab2830540ebeef7e443516bf5da413
john <john@linkhub.co.kr>
2025-08-05 17:26:38 +0900

    updated EasyFinBank API Example comment

--- EasyFinBankExample/views.py
+++ EasyFinBankExample/views.py
@@ -290,10 +290,10 @@ def requestJob(request):
         AccountNumber = ""
 
         # 시작일자, 날짜형식(yyyyMMdd)
-        SDate = "20241201"
+        SDate = "20250801"
 
         # 종료일자, 날짜형식(yyyyMMdd)
-        EDate = "20241231"
+        EDate = "20250831"
 
         result = easyFinBankService.requestJob(CorpNum, BankCode, AccountNumber, SDate, EDate)
 
@@ -306,12 +306,6 @@ def requestJob(request):
 def getJobState(request):
     """
     수집 요청(RequestJob API) 함수를 통해 반환 받은 작업 아이디의 상태를 확인합니다.
-    - 거래 내역 조회(Search API) 함수 또는 거래 요약 정보 조회(Summary API) 함수를 사용하기 전에
-    수집 작업의 진행 상태, 수집 작업의 성공 여부를 확인해야 합니다.
-    - 작업 상태(jobState) = 3(완료)이고 수집 결과 코드(errorCode) = 1(수집성공)이면
-    거래 내역 조회(Search) 또는 거래 요약 정보 조회(Summary) 를 해야합니다.
-    - 작업 상태(jobState)가 3(완료)이지만 수집 결과 코드(errorCode)가 1(수집성공)이 아닌 경우에는
-    오류메시지(errorReason)로 수집 실패에 대한 원인을 파악할 수 있습니다.
     - https://developers.popbill.com/reference/easyfinbank/python/api/job#GetJobState
     """
     try:
@@ -692,10 +686,10 @@ def getPaymentHistory(request):
         CorpNum = settings.testCorpNum
 
         # 조회 기간의 시작일자 (형식 : yyyyMMdd)
-        SDate = "20230101"
+        SDate = "20250801"
 
         # 조회 기간의 종료일자 (형식 : yyyyMMdd)
-        EDate = "20230131"
+        EDate = "20250831"
 
         # 목록 페이지번호 (기본값 1)
         Page = 1
@@ -724,10 +718,10 @@ def getUseHistory(request):
         CorpNum = settings.testCorpNum
 
         # 조회 기간의 시작일자 (형식 : yyyyMMdd)
-        SDate = "20230101"
+        SDate = "20250801"
 
         # 조회 기간의 종료일자 (형식 : yyyyMMdd)
-        EDate = "20230110"
+        EDate = "20250831"
 
         # 목록 페이지번호 (기본값 1)
         Page = 1
@@ -861,11 +855,10 @@ def joinMember(request):
     try:
         # 회원정보
         newMember = JoinForm(
-            # 아이디 (6자 이상 50자 미만)
+            # 아이디
             ID="join_id_test",
 
-            # 비밀번호 (8자 이상 20자 미만)
-            # 영문, 숫자, 특수문자 조합
+            # 비밀번호
             Password="password123!@#",
 
             # 사업자번호 "-" 제외
@@ -889,10 +882,10 @@ def joinMember(request):
             # 담당자 성명 (최대 100자)
             ContactName="담당자성명",
 
-            # 담당자 이메일주소 (최대 100자)
+            # 담당자 메일 (최대 100자)
             ContactEmail="",
 
-            # 담당자 연락처 (최대 20자)
+            # 담당자 휴대폰 (최대 20자)
             ContactTEL="",
         )
 
@@ -988,24 +981,23 @@ def registContact(request):
 
         # 담당자 정보
         newContact = ContactInfo(
-            # 아이디 (6자 이상 50자 미만)
+            # 아이디
             id="popbill_test_id",
 
-            # 비밀번호 (8자 이상 20자 미만)
-            # 영문, 숫자, 특수문자 조합
+            # 비밀번호
             Password="password123!@#",
 
-            # 담당자명 (최대 100자)
+            # 담당자 성명 (최대 100자)
             personName="담당자명",
 
-            # 담당자 연락처 (최대 20자)
+            # 담당자 휴대폰 (최대 20자)
             tel="",
 
-            # 담당자 이메일 (최대 100자)
+            # 담당자 메일 (최대 100자)
             email="",
 
-            # 담당자 조회권한, 1(개인) 2(읽기) 3(회사)
-            searchRole=1,
+            # 권한, 1(개인) 2(읽기) 3(회사)
+            searchRole=3,
         )
 
         response = easyFinBankService.registContact(CorpNum, newContact)
@@ -1088,20 +1080,20 @@ def updateContact(request):
 
         # 담당자 정보
         updateInfo = ContactInfo(
-            # 담당자 아이디
+            # 아이디
             id=UserID,
 
             # 담당자 성명 (최대 100자)
             personName="담당자_성명",
 
-            # 담당자 연락처 (최대 20자)
+            # 담당자 휴대폰 (최대 20자)
             tel="",
 
-            # 담당자 메일주소 (최대 100자)
+            # 담당자 메일 (최대 100자)
             email="",
 
-            # 담당자 조회권한, 1(개인) 2(읽기) 3(회사)
-            searchRole=1,
+            # 권한, 1(개인) 2(읽기) 3(회사)
+            searchRole=3,
         )
 
         response = easyFinBankService.updateContact(CorpNum, updateInfo)
@@ -1130,7 +1122,9 @@ def quitMember(request):
         UserID = settings.testUserID
 
         response = easyFinBankService.quitMember(CorpNum, QuitReason, UserID)
+
         return render(request, 'response.html', {"code": response.code, "message": response.message})
+
     except PopbillException as PE:
         return render(request, "exception.html", {"code": PE.code, "message": PE.message})
 
@@ -1150,7 +1144,9 @@ def getRefundInfo(request):
         UserID = settings.testUserID
 
         response = easyFinBankService.getRefundInfo(CorpNum,RefundCode,UserID)
+
         return render(request, 'getRefundInfo.html', {"code": response.code, "response": response})
+
     except PopbillException as PE:
         return render(request, "exception.html", {"code": PE.code, "message": PE.message})
 
@@ -1167,6 +1163,8 @@ def getRefundableBalance(request):
         UserID = settings.testUserID
 
         refundableBalance = easyFinBankService.getRefundableBalance(CorpNum, UserID)
+
         return render(request, 'getRefundableBalance.html', {"refundableBalance": refundableBalance})
+
     except PopbillException as PE:
         return render(request, "exception.html", {"code": PE.code, "message": PE.message})
